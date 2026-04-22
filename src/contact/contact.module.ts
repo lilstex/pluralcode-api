79154b701142e15d9625adb20ece7b26f563021c
Emmanuel C Mbagwu <lilstex4good@gmail.com>
2026-04-22 16:52:35 +0100

    Created the contact us module 2

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContactController } from './controller/contact.controller';
 import { ContactService } from './service/contact.service';
+import { EmailModule } from 'src/providers/email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [ContactController],
   providers: [ContactService],
 })
